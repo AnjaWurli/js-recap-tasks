661d4b4089595d29f6b6ac0b4c6c33ef6ebe9e03
Anja Wurlitzer <anja.wurlitzer.coding@gmail.com>
2023-01-13 11:32:55 +0100

    second solution for filter-api-data

--- filter-api-data.js
+++ filter-api-data.js
@@ -3,28 +3,13 @@ function filterApiData(apiData, mandatoryKeys) {
   //mandatoryKeys=[key1,key2,...]
   //return Array with the Objects that passed the test
 
-  let result = [];
-
-  for (let entry of apiData) {
-    if (
-      mandatoryKeys.every((currentKey) =>
-        Object.keys(entry).includes(currentKey)
-      )
-    ) {
-      result.push(entry);
-    }
-  }
-  return result;
-
-  /*
   let result = apiData.filter((entry) => {
     //Object.keys(entry) //[key1,key2,..]
-    
-      mandatoryKeys.every((currentKey) =>
+
+    return mandatoryKeys.every((currentKey) =>
       Object.keys(entry).includes(currentKey)
     );
   });
 
   return result;
-  */
 }
